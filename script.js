e2a5a6ebca19981846711ffeb801e40b04e00abd
thushitharan <thushitharansutha@gmail.com>
2023-04-24 16:25:24 +0530

    Minor Upgrades

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//IP VALIDATION
 function ValidateIPaddress(ipaddress)
  {
 	var ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
@@ -15,7 +16,7 @@ function ValidateIPaddress(ipaddress)
 }
 
 
-//Date
+//DATE
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
@@ -29,3 +30,15 @@ var t = new Date();
 
 var datestring = d.getHours() + " : " + d.getMinutes();
 document.getElementById("time").innerHTML = datestring;
+
+
+//MODAL
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
